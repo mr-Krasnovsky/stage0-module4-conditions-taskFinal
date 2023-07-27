c93df4e549bd04077072f13005006ab6463bc4ca
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-07-27 16:15:06 +0300

    Complited

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        String quadrant = (x == 0 && y == 0) ? "zero" :
+                (x > 0 && y > 0) ? "first" :
+                        (x < 0 && y > 0) ? "second" :
+                                (x < 0 && y < 0) ? "third" :
+                                        (x > 0 && y < 0) ? "fourth" : "unknown";
+        System.out.println(quadrant);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,40 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+    boolean leapYear = isLeapYear(year);
+            switch (month) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    System.out.println(31);
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    System.out.println(30);
+                    break;
+                case 2:
+                    int feb = leapYear == true ? 29 : 28;
+                    System.out.println(feb);
+                    break;
+                default:
+                    System.out.println("invalid date");
+            }
+    }
 
+    public boolean isLeapYear(int year) {
+        boolean isLeap = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 != 0 || year % 400 == 0) {
+                isLeap = true;
+            }
+        }
+        return isLeap;
     }
 }
